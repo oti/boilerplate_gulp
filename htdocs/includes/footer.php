04d1ffa4df746b5b7b11b5c282e54691ec9f5923
oti <otiext@gmail.com>
2016-01-21 17:44:12 +0900

    `gulp jade:php`

---
+++ htdocs/includes/footer.php
@@ -0,0 +1,6 @@
+
+<footer>
+  <div class="global-footer">
+    <p class="footer-copyright">Copyright © 2016 hugahoge, All Rights Reserved.</p>
+  </div>
+</footer>
---
+++ htdocs/includes/header.php
@@ -0,0 +1,12 @@
+
+<header role="banner">
+  <div class="global-header">
+    <nav>
+      <ul class="header-nav">
+        <li class="header-nav__item"><a href="/" class="header-nav-link">HOME</a></li>
+        <li><a href="/archive" class="header-nav-link">ARCHIVE</a></li>
+        <li><a href="/about" class="header-nav-link">ABOUT</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
