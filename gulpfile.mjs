148f0c2601587ab0620f33e334491c9a3f44295c
oti <otiext@gmail.com>
2022-02-13 14:12:34 +0900

    feat(task): gulpfile.mjs, change to include config

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,22 +1,45 @@
 import gulp from "gulp";
-import { server } from "./task/server.mjs";
+
+import { serve } from "./task/serve.mjs";
 import { html } from "./task/html.mjs";
 import { style } from "./task/style.mjs";
 import { script } from "./task/script.mjs";
 import { image } from "./task/image.mjs";
-import { path } from "./config.json";
 
-// watch
-const watch = (done) => {
-  gulp.watch([path.src.html_src, "./config.json"], gulp.parallel(html));
-  gulp.watch([path.src.style], gulp.parallel(style));
-  gulp.watch([path.src.script], gulp.parallel(script));
-  gulp.watch([path.src.image], gulp.parallel(image));
+const path = {
+  doc_root: "dist/",
+  src: {
+    html: "src/html/**/*.pug",
+    html_ignore: "!src/html/_partial/**.pug",
+    style: "src/style/**/*.scss",
+    script: "src/script/**/*.js",
+    image: "src/image/**/*.{jpg,png,gif,svg}",
+  },
+  dest: {
+    html: "dist/",
+    style: "dist/asset/style",
+    script: "dist/asset/script",
+    image: "dist/asset/image",
+  },
+};
+
+export const b = (done) => serve(done, path.doc_root);
+export const h = () =>
+  html([path.src.html, path.src.html_ignore], path.dest.html);
+export const c = () => style(path.src.style, path.dest.style);
+export const i = () => image(path.src.image, path.dest.image);
+export const s = () => script(undefined, path.dest.script);
+
+export const w = (done) => {
+  gulp.watch([path.src.html], h);
+  gulp.watch([path.src.style], c);
+  gulp.watch([path.src.script], i);
+  gulp.watch([path.src.image], s);
   done();
 };
 
-// npx gulp
-gulp.task("default", gulp.series(html, style, script, image, watch, server));
-
 // npx gulp build
-gulp.task("build", gulp.series(html, style, script, image));
+export const build = gulp.series(h, c, i, s);
+
+// npx gulp
+export default gulp.series(build, w, b);
