e10f01e2fb4a6383d6692bb996e87e6e8c2ef7b6
oti <otiext@gmail.com>
2022-02-13 15:20:53 +0900

    feat(task): html.mjs, change to include `locals` define

--- task/html.mjs
+++ task/html.mjs
@@ -3,15 +3,24 @@ import browserSync from "browser-sync";
 import pug from "gulp-pug";
 import plumber from "gulp-plumber";
 
+const locals = {
+  _siteName: "Gulp Web Dev Boilerplate",
+  _siteDesc: "Gulp を使ったウェブサイト開発環境のボイラープレートです。",
+  _siteKeywords: "boilerplate, template, webdev, gulp",
+  _siteHost: "https://exmaple.com",
+  _sitePath: "",
+  _siteOGP: "/asset/image/ogp.png",
+  _siteStyle: "/asset/style/style.css",
+  _siteScript: "/asset/script/script.js",
+};
+
 export const html = (src, dest) => {
   return gulp
     .src(src)
     .pipe(plumber())
     .pipe(
       pug({
-        locals: {
-          // pug に値を渡したい時はここに。
-        },
+        locals,
         pretty: true,
       })
     )
