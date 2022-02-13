52b37e3319e836a8c2d28a0dec738c3632ccfea4
oti <otiext@gmail.com>
2022-02-13 13:43:47 +0900

    feat(task): script.mjs, change usage `gulp-uglify-es`

--- task/script.mjs
+++ task/script.mjs
@@ -2,16 +2,18 @@ import gulp from "gulp";
 import browserSync from "browser-sync";
 import plumber from "gulp-plumber";
 import babel from "gulp-babel";
-import uglify from "gulp-uglify-es";
+import uglifyEs from "gulp-uglify-es";
 import webpackStream from "webpack-stream";
 import webpack from "webpack";
-import webpackConfig from "../webpack.config.js";
+import webpackConfig from "../webpack.config.mjs";
+
+const uglify = uglifyEs.default;
 
 export const script = (_, dest) => {
   return plumber()
     .pipe(webpackStream(webpackConfig, webpack))
     .pipe(babel())
-    .pipe(uglify({}))
+    .pipe(uglify())
     .pipe(gulp.dest(dest))
     .pipe(
       browserSync.reload({
