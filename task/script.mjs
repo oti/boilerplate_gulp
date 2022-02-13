7c4f94ebe4c32229877b53d383a4dda309fa0888
oti <otiext@gmail.com>
2022-02-13 13:43:37 +0900

    feat(task): script.mjs, change to using argument

--- task/script.mjs
+++ task/script.mjs
@@ -6,14 +6,13 @@ import uglify from "gulp-uglify-es";
 import webpackStream from "webpack-stream";
 import webpack from "webpack";
 import webpackConfig from "../webpack.config.js";
-import { path } from "../config.json";
 
-export const script = () => {
+export const script = (_, dest) => {
   return plumber()
     .pipe(webpackStream(webpackConfig, webpack))
     .pipe(babel())
     .pipe(uglify({}))
-    .pipe(gulp.dest(path.dist.script))
+    .pipe(gulp.dest(dest))
     .pipe(
       browserSync.reload({
         stream: true,
