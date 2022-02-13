946a3df69522341d876c7006e996d5ee9d640c88
oti <otiext@gmail.com>
2022-02-13 14:12:00 +0900

    feat(task): serve.mjs, change to using argument

---
+++ task/serve.mjs
@@ -0,0 +1,12 @@
+import browserSync from "browser-sync";
+
+export const serve = (done, baseDir) => {
+  browserSync.init(
+    {
+      server: {
+        baseDir,
+      },
+    },
+    done
+  );
+};
