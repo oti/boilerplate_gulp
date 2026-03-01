bf265362e6bf6dcf197681eebc0d5ec730870bdc
oti <otiext@gmail.com>
2026-03-01 22:44:22 +0900

    feat(script): CSS の `scroll-behavior: smooth` で代用可能になったので `smooth-scroll` パッケージを使っていた AnchorLink モジュールを削除した

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,12 @@
 import uac from "./libs/user-agent-checker";
 uac();
 
-import AnchorLink from "./modules/AnchorLink";
 import Disclosure from "./modules/Disclosure";
 import Drawer from "./modules/Drawer";
 import InView from "./modules/InView";
 import Tab from "./modules/Tab";
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  const anchorLink = new AnchorLink();
-  anchorLink.init();
-
   document.querySelectorAll(".p-footerNav__icon").forEach((button) => {
     const target = document.querySelector(
       "#" + button.getAttribute("aria-controls"),
